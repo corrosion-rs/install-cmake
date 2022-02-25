0246327dfcc315330c1d045131d2ef1f2773ba3a
Jonathan Schwender <schwenderjonathan@gmail.com>
2022-02-25 22:11:21 +0100

    test: check multiple cmake versions
    The artifact names changed between cmake 3.19 and 3.20.
    
    Anyway, it makes sense to test multiple versions,
    so lets just add that.

--- __tests__/cmake.test.ts
+++ __tests__/cmake.test.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs'
+import * as fs from 'fs';
 import * as process from 'process';
 import * as path from 'path';
 import * as io from '@actions/io';
@@ -9,44 +9,47 @@ const tempDirectory = path.join(__dirname, 'temp-get-cmake');
 
 jest.setTimeout(30 * 1000);
 
-describe('get-cmake', () => {
-    beforeEach(async () => {
-        await io.rmRF(tempDirectory);
-        await io.mkdirP(tempDirectory);
-        Object.keys(process.env)
-            .filter(key => key.match(/^INPUT_/))
-            .forEach(key => {
-                delete process.env[key];
-            });
-        const gh_path = path.join(tempDirectory, 'gh-path');
-        fs.writeFileSync(gh_path, "");
-        process.env.GITHUB_PATH = gh_path;
-        process.env.INPUT_CMAKE = '3.17.2';
-        process.env.GITHUB_WORKSPACE = tempDirectory;
-        process.env.RUNNER_TEMP = path.join(tempDirectory, 'temp');
-        process.env.RUNNER_TOOL_CACHE = path.join(
-            tempDirectory,
-            'tempToolCache'
-        );
-    });
-
-    afterAll(async () => {
-        try {
+describe.each([['3.12.4'], ['3.18.0'], ['3.19.0'], ['3.20.0']])(
+    `get-cmake %s`,
+    cmake_version => {
+        beforeEach(async () => {
             await io.rmRF(tempDirectory);
-        } catch {
-            console.log('Failed to remove test directories');
-        }
-    }, 100000);
-
-    it('should download CMake', async () => {
-        const cmakePath = await cmake();
-        expect(cmakePath).toBeDefined();
-        expect(cmakePath).not.toHaveLength(0);
-
-        const { status, error } = spawn(cmakePath, ['--version'], {
-            encoding: 'utf8'
+            await io.mkdirP(tempDirectory);
+            Object.keys(process.env)
+                .filter(key => key.match(/^INPUT_/))
+                .forEach(key => {
+                    delete process.env[key];
+                });
+            const gh_path = path.join(tempDirectory, 'gh-path');
+            fs.writeFileSync(gh_path, '');
+            process.env.GITHUB_PATH = gh_path;
+            process.env.GITHUB_WORKSPACE = tempDirectory;
+            process.env.INPUT_CMAKE = cmake_version;
+            process.env.RUNNER_TEMP = path.join(tempDirectory, 'temp');
+            process.env.RUNNER_TOOL_CACHE = path.join(
+                tempDirectory,
+                'tempToolCache'
+            );
         });
-        expect(error).toBeUndefined();
-        expect(status).toBe(0);
-    });
-});
+
+        afterAll(async () => {
+            try {
+                await io.rmRF(tempDirectory);
+            } catch {
+                console.log('Failed to remove test directories');
+            }
+        }, 100000);
+
+        it(`should download CMake ${cmake_version}`, async () => {
+            const cmakePath = await cmake();
+            expect(cmakePath).toBeDefined();
+            expect(cmakePath).not.toHaveLength(0);
+
+            const { status, error } = spawn(cmakePath, ['--version'], {
+                encoding: 'utf8'
+            });
+            expect(error).toBeUndefined();
+            expect(status).toBe(0);
+        });
+    }
+);
